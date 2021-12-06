595c916a46f33de81912ccb924111a8310a91668
Autuan.Yu <autuan.yu@gmail.com>
2021-12-06 20:27:58 +0800

    init project

---
+++ src/main/java/top/autuan/domainchange/DomainChangeApplication.java
@@ -0,0 +1,13 @@
+package top.autuan.domainchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainChangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/autuan/domainchange/DomainChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package top.autuan.domainchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainChangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
