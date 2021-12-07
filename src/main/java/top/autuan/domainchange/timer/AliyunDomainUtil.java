58035c524b190f933402db9c92d0ac640f43fbe5
AutuanYu <yu@autuan.top>
2021-12-07 17:04:09 +0800

    updateDomainRecord 实现

--- src/main/java/top/autuan/domainchange/timer/AliyunDomainUtil.java
+++ src/main/java/top/autuan/domainchange/timer/AliyunDomainUtil.java
@@ -14,51 +14,72 @@ import lombok.SneakyThrows;
 import java.rmi.ServerException;
 
 public class AliyunDomainUtil {
+    private static Client client;
+
+    static {
+        try {
+            client = new Client(new Config()
+                    // 您的AccessKey ID
+                    .setAccessKeyId(FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\key.txt"))
+                    // 您的AccessKey Secret
+                    .setAccessKeySecret( FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\secret.txt")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    @SneakyThrows
+//    public AliyunDomainUtil() {
+//        // https://next.api.aliyun.com/api-tools/sdk/Alidns?spm=a2c4g.11186623.0.0.30bc12d4kjja9M&version=2015-01-09&language=java-tea
+//        String accessKeyId = "";
+//        accessKeyId = FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\key.txt");
+//        String accessKeySecret = "";
+//        accessKeySecret = FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\secret.txt");
+//
+//        System.out.println(accessKeyId);
+//        System.out.println(accessKeySecret);
+//
+//
+//        Config config = new Config()
+//                // 您的AccessKey ID
+//                .setAccessKeyId(accessKeyId)
+//                // 您的AccessKey Secret
+//                .setAccessKeySecret(accessKeySecret);
+//        // 访问的域名
+//        config.endpoint = "alidns.cn-hangzhou.aliyuncs.com";
+//
+//
+//        client = new Client(config);
+//    }
+
     // todo modifyProd  方法
     // todo modifyBackup 方法
     // todo config single
 
-    // todo 实现
-    public static void updateDomainRecord(String recordId,String prefix,String value){
-//        String recordId = "";
-//        String prefix = "";
-
+    @SneakyThrows
+    public static void updateDomainRecord(String recordId, String prefix, String value){
+        UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest()
+                .setRecordId(recordId)
+                .setType("A")
+                .setValue(value)
+                .setRR(prefix);
+        UpdateDomainRecordResponse updateDomainRecordResponse = client.updateDomainRecord(updateDomainRecordRequest);
     }
 
     @Deprecated
     @SneakyThrows
     public static void main(String[] args) {
-        // https://next.api.aliyun.com/api-tools/sdk/Alidns?spm=a2c4g.11186623.0.0.30bc12d4kjja9M&version=2015-01-09&language=java-tea
-        String accessKeyId = "";
-        accessKeyId = FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\key.txt");
-        String accessKeySecret = "";
-        accessKeySecret = FileUtil.readUtf8String("F:\\OneDrive\\企岛\\domain\\secret.txt");
 
-        System.out.println(accessKeyId);
-        System.out.println(accessKeySecret);
-
-
-        Config config = new Config()
-                // 您的AccessKey ID
-                .setAccessKeyId(accessKeyId)
-                // 您的AccessKey Secret
-                .setAccessKeySecret(accessKeySecret);
-        // 访问的域名
-        config.endpoint = "alidns.cn-hangzhou.aliyuncs.com";
-
-
-        Client client = new Client(config);
-
-//        UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
-        UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest()
-                .setRecordId("21553798018569216")
-                .setType("A")
-                .setValue("47.114.72.89")
-                .setRR("test");
+        updateDomainRecord("21553798018569216","test","47.114.72.89");
+//        UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest()
+//                .setRecordId("21553798018569216")
+//                .setType("A")
+//                .setValue("47.102.145.80")
+//                .setRR("test");
 //        updateDomainRecordRequest
         // 复制代码运行请自行打印 API 的返回值
-        UpdateDomainRecordResponse updateDomainRecordResponse = client.updateDomainRecord(updateDomainRecordRequest);
-        System.out.println(JSONUtil.toJsonStr(updateDomainRecordResponse));
+//        UpdateDomainRecordResponse updateDomainRecordResponse = client.updateDomainRecord(updateDomainRecordRequest);
+//        System.out.println(JSONUtil.toJsonStr(updateDomainRecordResponse));
 
     }
 
